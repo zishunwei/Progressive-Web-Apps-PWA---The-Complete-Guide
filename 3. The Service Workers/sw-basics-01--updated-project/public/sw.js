010ae5b68d2f8a78653a4b62db92c3b35e0303dc
zishun <uceszwe@ucl.ac.uk>
2020-06-20 16:23:16 +0100

    update

---
+++ 3. The Service Workers/sw-basics-01--updated-project/public/sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', function (event) {
+        console.log('[Service Worker] Installing Service Worker ...', event);
+    }
+);
+
+self.addEventListener('activate', function (event) {
+        console.log('[Service Worker] Activating Service Worker ...', event);
+        return self.clients.claim()
+    //Clients 接口的  claim() 方法允许一个激活的 service worker 将自己设置为其 scope 内所有clients 的controller .
+    // 这会在由此service worker 控制的任何 clients 中触发 navigator.serviceWorker  上的  "controllerchange"  事件.
+    }
+);
